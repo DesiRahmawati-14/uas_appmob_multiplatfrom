099c929302c581f939a881bd731748a4b744a04f
Desi Rahmawati <rahmawatidesi730@gmail.com>
2021-06-28 22:22:59 +0700

    main

--- uas/lib/main.dart
+++ uas/lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:uas/components/customListTile.dart';
+import 'package:uas/services/api_service.dart';
+import 'package:uas/model/article_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,20 +19,39 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage>{
+  get client => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Desi Rahmawati", style: TextStyle(color: Colors.black)),
-        backgroundColor: Colors.blue,
+          backgroundColor: Colors.blue,
       ),
-
+        body: FutureBuilder(
+          future: buildClient().getArticle(),
+          builder: (BuildContext context, AsyncSnapshot<List<Article>> snapsht){
+            var snapshot;
+                        if (snapshot.hasData) {
+              List<Article> articles = snapshot.data;
+            return ListView.builder(
+              itemCount: articles.length,
+              itemBuilder: (context, index) =>
+                  customListTile(articles[index], context),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+          },
+        ),
     ) ;
   }
+
+  buildClient() => client;
 }
